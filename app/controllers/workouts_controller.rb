0433b2e55d41b4163ca4180802ddafd571bcfed7
ToniRib <tonimarierib@gmail.com>
2016-01-31 13:02:00 -0700

    Refactor WorkoutCreator to make WorkoutController slim

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,20 +6,22 @@ class WorkoutsController < ApplicationController
   def create
     token = OAuth2::AccessToken.new(mmf_client, session[:auth_token])
 
-    offset = 0
-    next_flag = false
-    until next_flag do
-      workouts = token.get("/v7.0/workout/?user=#{current_user.map_my_fitness_id}",
-                           :params => { limit: 40, offset: offset },
-                           :headers => { 'Api-Key' => ENV['MMF_API_KEY'],
-                                        'Authorization' => session[:auth_token] }).body
+    WorkoutCreator.new(token, session[:auth_token], current_user.id)
 
-      workout_data = JSON.parse(workouts)
-      WorkoutCreator.parse_and_create(workout_data["_embedded"]["workouts"], current_user.id)
-
-      offset += 40
-      next_flag = true if workout_data["_links"]["next"].nil?
-    end
+    # offset = 0
+    # next_flag = false
+    # until next_flag do
+    #   workouts = token.get("/v7.0/workout/?user=#{current_user.map_my_fitness_id}",
+    #                        :params => { limit: 40, offset: offset },
+    #                        :headers => { 'Api-Key' => ENV['MMF_API_KEY'],
+    #                                     'Authorization' => session[:auth_token] }).body
+    #
+    #   workout_data = JSON.parse(workouts)
+    #   WorkoutCreator.parse_and_create(workout_data["_embedded"]["workouts"], current_user.id)
+    #
+    #   offset += 40
+    #   next_flag = true if workout_data["_links"]["next"].nil?
+    # end
 
     redirect_to workouts_path
   end
--- app/models/workout_creator.rb
+++ app/models/workout_creator.rb
@@ -1,5 +1,32 @@
 class WorkoutCreator
-  def self.parse_and_create(workouts, user_id)
+  attr_reader :token, :auth_token, :user
+
+  def initialize(token, auth_token, user_id)
+    @token = token
+    @auth_token = auth_token
+    @user = User.find(user_id)
+
+    create_workouts
+  end
+
+  def create_workouts
+    offset = 0
+    next_flag = false
+    until next_flag do
+      workouts = token.get("/v7.0/workout/?user=#{@user.map_my_fitness_id}",
+                           :params => { limit: 40, offset: offset },
+                           :headers => { 'Api-Key' => ENV['MMF_API_KEY'],
+                                         'Authorization' => auth_token }).body
+
+      workout_data = JSON.parse(workouts)
+      parse_and_create(workout_data["_embedded"]["workouts"])
+
+      offset += 40
+      next_flag = true if workout_data["_links"]["next"].nil?
+    end
+  end
+
+  def parse_and_create(workouts)
     workouts.each do |workout|
       next if workout["aggregates"]["active_time_total"].zero?
 
@@ -13,7 +40,7 @@ class WorkoutCreator
         total_distance: workout["aggregates"]["distance_total"],
         average_speed: workout["aggregates"]["speed_avg"],
         total_elapsed_time: workout["aggregates"]["elapsed_time_total"],
-        user_id: user_id
+        user_id: user.id
       )
     end
   end
